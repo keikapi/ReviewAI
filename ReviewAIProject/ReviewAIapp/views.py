6af03c92c608367fb40b9de78ded9163b20cefd6
KeigoTakagi <keigo.0390@gmail.com>
2024-06-26 18:14:17 +0900

    6/26

--- ReviewAIProject/ReviewAIapp/views.py
+++ ReviewAIProject/ReviewAIapp/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from .forms import userpromptForm
 
+import openai
+
 class IndexView(generic.TemplateView):
     template_name = "top.html"
 
-
+reviewpers =[]
+reviewcont = []
 def userpromptview(request):
     if request.method == 'POST':
         form = userpromptForm(request.POST)
@@ -16,8 +19,8 @@ def userpromptview(request):
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
-            reviewpers=[]
-            reviewcont=request.POST.get("reviewcont")
+            #reviewpers=[]
+            reviewcont.append(request.POST.get("reviewcont"))
             if request.POST.get("reviewpers_1") == "on":
                 reviewpers.append("信頼性")
             if request.POST.get("reviewpers_2") == "on":
@@ -29,7 +32,7 @@ def userpromptview(request):
             if request.POST.get("reviewpers_5") == "on":
                 reviewpers.append("誤字脱字")
 
-            print(reviewpers[1])
+            print(reviewpers)
             # redirect to a new URL:
             return HttpResponseRedirect("redirect")
 
@@ -39,9 +42,31 @@ def userpromptview(request):
 
     return render(request, "userprompt.html", {"form": form})
 
-
-
 def redirectview(request):
+    for r in reviewpers:
+        print(r)
+    print(reviewcont[0])
+    openai.api_type = "azure"
+    openai.api_base = "https://reviewmodel.openai.azure.com/" 
+    openai.api_version = "2023-03-15-preview"
+    openai.api_key = "27e6b907565140a79830da866aec6908"
+    response = openai.ChatCompletion.create(
+        engine="ReviewAI", # モデルのデプロイ名を指定
+        messages = [
+            {"role":"system",
+            "content":"## 役割\nあなたは、ベテランエンジニアで、私の上司です。クライアントにアウトプットする前に、あなたに対して内部レビューを依頼しています。ユーザーが提供するレビュー依頼文章に対して、特定のレビュー観点を用いて、レビューしてください。レビュー観点/方針については、以下を参照してください。\n## レビュー観点/方針\n"+','.join(reviewpers)},
+            # レビュー依頼文面を追加
+            {"role":"user",
+            "content":reviewcont[0]}
+            ],
+        temperature=1,
+        max_tokens=800,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None)
+    print(','.join(reviewpers))
+    print(response["choices"][0]["message"]["content"]) # 結果を出力する
 
     return render(request, "redirect.html")
 
